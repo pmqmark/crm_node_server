f6f0ae0ea1d6d6abcf7bbbb90949e4db1e955a4b
Seion03 <seionshoji2003@gmail.com>
2025-02-13 18:15:06 +0530

    Added client creation and control error rectified

--- src/models/interfaces/user.interface.ts
+++ src/models/interfaces/user.interface.ts
@@ -1,17 +1,18 @@
 import { Types } from 'mongoose';
-import { CreateUserDto } from '../dtos/user.dto';
 
-export interface IClient extends CreateUserDto {
-    position: string;
-    packageDetails: string;
-    createdAt: Date;
-    updatedAt: Date;
-    isActive: boolean;
+export interface IUser {
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+  roles:"admin"|"Employee",
+  isActive: boolean;
 }
 
-export interface ValidatedClient {
-    _id: Types.ObjectId;
-    email: string;
-    position: string;
-    packageDetails: string;
+
+export interface ValidatedUser {
+_id: Types.ObjectId;
+email: string;
+
+roles:string
 }
