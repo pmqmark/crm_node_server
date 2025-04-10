7ef4aa380b527116cfa75849d2184cc4d30ea853
Manu-Madhu <manum66466@gmail.com>
2025-04-10 19:05:15 +0530

    Basic docker file config + docker image creation completed

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
-import express, { NextFunction, Request,Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import dotenv from 'dotenv';
 import path from 'path';
 const CookieParser = require("cookie-parser");
 const cors = require("cors");
-const db=require("./db")
+const db = require("./db")
 
-dotenv.config({ 
-    path: path.resolve(__dirname, '.env')
-  });
+dotenv.config();
 
-import  adminRouter from "./routes/admin";
+import adminRouter from "./routes/admin";
 import authRouter from "./routes/auth"
 import employeerouter from "./routes/employee"
 import clientRouter from "./routes/client";
 import { generateAccessToken, generateRefreshToken } from "./middleware/tokenMiddleware";
 
 
-const app=express()
+const app = express()
 app.use(cors({
   origin: '*',                // Allow all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],  // Allow all common HTTP methods
@@ -27,17 +25,17 @@ db.connect()
 app.use(CookieParser())
 app.use(express.json()); // ✅ Parses JSON bodies
 app.use(express.urlencoded({ extended: true })); // ✅ Parses URL-encoded bodies
-app.use('/api/admin',adminRouter)
-app.use('/api/auth',authRouter)
-app.use('/api/employee',employeerouter)
+app.use('/api/admin', adminRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/employee', employeerouter)
 app.use('/api/client', clientRouter)
 
 
-const PORT=process.env.PORT||3000;
+const PORT = process.env.PORT || 3000;
 console.log(PORT)
 
-app.listen(PORT,()=>{
-    // console.log(generateAccessToken(dummyUser))
-    console.log(`Running on port ${PORT}`);
+app.listen(PORT, () => {
+  // console.log(generateAccessToken(dummyUser))
+  console.log(`Running on port ${PORT}`);
 });
 
