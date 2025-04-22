0b0ecfa1f3bece57a20a2cfa1a218b89f0ade93b
abhijth.r.b <abhijithrb91@gmail.com>
2025-04-22 15:58:15 +0530

    regenerate-tokens API added

--- src/controllers/authcontroller.ts
+++ src/controllers/authcontroller.ts
@@ -5,15 +5,16 @@ import { CreateUserDto, loginUserDto, validitatedUser } from "../dtos/userdto"; 
 import { generateAccessToken, generateRefreshToken } from "../middleware/tokenMiddleware";
 import Admin from "../models/admin";
 import { CreateAdminDto } from "../dtos/admindto";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 export class AuthService {
-  async createAdmin (req: Request, res: Response): Promise<Response>{
+  async createAdmin(req: Request, res: Response): Promise<Response> {
     try {
       const adminData: CreateAdminDto = req.body;
-      
+
       // Hash password
       const hashedPassword = await bcrypt.hash(adminData.password, 10);
-      
+
       // Create new admin
       const admin = new Admin({
         email: adminData.email,
@@ -23,62 +24,103 @@ export class AuthService {
         createdAt: new Date(),
         lastLogin: null,
       });
-  
+
       const savedAdmin = await admin.save();
-  
+
       return res.status(201).json({
         message: "Admin created successfully",
         admin: savedAdmin,
       });
-  
+
     } catch (error: unknown) {
       if (error instanceof Error) {
-        return res.status(500).json({ 
-          message: `Error creating admin: ${error.message}` 
+        return res.status(500).json({
+          message: `Error creating admin: ${error.message}`
         });
       }
-      return res.status(500).json({ 
-        message: "An unknown error occurred while creating admin" 
+      return res.status(500).json({
+        message: "An unknown error occurred while creating admin"
       });
     }
-};
+  };
 
 
 
-  async login(req: Request, res: Response): Promise<validitatedUser|Response> {
+  async login(req: Request, res: Response): Promise<validitatedUser | Response> {
     try {
       const userData: loginUserDto = req.body;
-      const {email,password}=userData
+      const { email, password } = userData
 
-        const usr = await User.findOne({ email }); 
-          
-        if (!usr) {
+      const usr = await User.findOne({ email });
+
+      if (!usr) {
         throw new Error('Invalid credentials');
-        }
-        const isPasswordValid = await bcrypt.compare(password, usr.password);
-        if (!isPasswordValid) {
-                throw new Error('Invalid credentials');
-        }
+      }
+      const isPasswordValid = await bcrypt.compare(password, usr.password);
+      if (!isPasswordValid) {
+        throw new Error('Invalid credentials');
+      }
 
-        
 
-        const verifieduser:validitatedUser={
-            id:usr._id,
-            email:usr.email,
-            role:usr.role
-        }
 
-        const accesstoken =generateAccessToken(verifieduser)
-        const refreshtoken=generateRefreshToken(verifieduser)
+      const verifieduser: validitatedUser = {
+        id: usr._id,
+        email: usr.email,
+        role: usr.role
+      }
 
-          
-        return res.status(200).json({
-            message: "Login successful",
-            user: verifieduser,
-            accesstoken,
-            refreshtoken
-        });
-      
+      const accesstoken = generateAccessToken(verifieduser)
+      const refreshtoken = generateRefreshToken(verifieduser)
+
+
+      return res.status(200).json({
+        message: "Login successful",
+        user: verifieduser,
+        accesstoken,
+        refreshtoken
+      });
+
+
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(500).json({ message: `Error creating user: ${error.message}` });
+      }
+      return res.status(500).json({ message: "An unknown error occurred while creating user" });
+    }
+  }
+
+  async regenerateTokens(req: Request, res: Response): Promise<Response> {
+    try {
+      const { refreshToken } = req.body
+
+      if (typeof refreshToken !== 'string') {
+        return res.status(400).json({
+          success: false,
+          message: 'Invalid Token',
+          data: null,
+          error: 'BAD_REQUEST'
+        })
+      }
+
+      const decoded: any = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET as string)
+      console.log({ decoded })
+
+      const verifieduser: validitatedUser = {
+        id: decoded.id,
+        email: decoded.email,
+        role: decoded.role
+      }
+
+      const accessToken = generateAccessToken(verifieduser);
+
+      const rt = generateRefreshToken(verifieduser)
+
+      return res.status(200).json({
+        success: true,
+        message: 'success',
+        data: { accessToken, refreshToken: rt },
+        error: null
+      });
 
     } catch (error: unknown) {
       if (error instanceof Error) {
@@ -90,11 +132,11 @@ export class AuthService {
 
 
 
-  
+
 }
 
-      
 
 
-  
+
+
 
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { AuthService } from "../controllers/authcontroller";
-import {  authMiddleware } from '../middleware/verifyToken';
-import {AuthRequest}  from '../middleware/verifyToken'
+import { authMiddleware } from '../middleware/verifyToken';
+import { AuthRequest } from '../middleware/verifyToken'
 import { roleGuard } from '../middleware/roleguard';
 const authService = new AuthService();
 const router = Router();
@@ -11,19 +11,21 @@ router.post('/create-admin', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-    
+
     await authService.login(req, res);
 });
 
 
 
-router.get('/protected', 
-    authMiddleware,roleGuard(["admin"]),(req:AuthRequest,res)=>{
+router.get('/protected',
+    authMiddleware, roleGuard(["admin"]), (req: AuthRequest, res) => {
         res.status(201).json({ message: req.user })
     }
-  );
+);
 
-
+router.post('/regenerate-tokens', async (req, res) => {
+    await authService.regenerateTokens(req, res);
+});
 
 
 export default router;
