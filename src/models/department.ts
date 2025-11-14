ae9a93c591cc64f1f0bacf490dffadecf727db1a
muhammed shafir <muhammedshafirweb@gmail.com>
2025-11-14 14:05:00 +0530

    department dulicate creation fixed

--- src/models/department.ts
+++ src/models/department.ts
@@ -1,26 +1,27 @@
-import mongoose, { Schema, Document } from 'mongoose';
-import {IDepartment} from "../dtos/departmentdto"
+import mongoose, { Schema, Document } from "mongoose";
+import { IDepartment } from "../dtos/departmentdto";
 
 const departmentSchema = new Schema<IDepartment>({
-    name: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    manager_id: {
-      type: Schema.Types.ObjectId,
-      ref: 'Employee',
-      required: false,
-    },
-    created_at: {
-      type: Date,
-      default: Date.now,
-    },
-  });
+  name: {
+    type: String,
+    required: true,
+    unique: true, // enforces one department name across the collection
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  manager_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Employee",
+    required: false,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const Department = mongoose.model<IDepartment>('Department', departmentSchema);
+const Department = mongoose.model<IDepartment>("Department", departmentSchema);
 
 export default Department;
