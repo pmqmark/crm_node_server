b31ca1dea6113f030cfff69747144de0300f9f49
muhammed shafir <muhammedshafirweb@gmail.com>
2025-11-19 20:20:36 +0530

    db bug fix

--- src/db.ts
+++ src/db.ts
@@ -2,14 +2,12 @@ import mongoose from "mongoose";
 
 export const connect = (): void => {
   // Local MongoDB connection URL
-  // if (!process.env.DATABASE_URL) {
-  //   throw new Error("DATABASE_URL is not defined in environment configuration");
-  // }
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined in environment configuration");
+  }
 
   mongoose
-    .connect(
-      "mongodb+srv://qmarktechnolabs:dIpFRkxVBVPgPWN1@cluster0.typywei.mongodb.net/crm_master"
-    )
+    .connect(process.env.DATABASE_URL)
     .then(() => {
       console.log("Database connected");
     })
