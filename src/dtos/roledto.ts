565a69a32719ad75803f4dfd8b66b3159f8b9c6f
muhammed shafir <muhammedshafirweb@gmail.com>
2025-12-12 21:35:48 +0530

    role soft delete and only retreve active roles

--- src/dtos/roledto.ts
+++ src/dtos/roledto.ts
@@ -1,8 +1,9 @@
 import { Schema } from "mongoose";
 
 export interface IRole extends Document {
-    name: string;
-    description: string;
-    permissions?: string[];
-     // Optional - can be null for global roles
-  }
+  name: string;
+  description: string;
+  permissions?: string[];
+  isActive: boolean;
+  // Optional - can be null for global roles
+}
